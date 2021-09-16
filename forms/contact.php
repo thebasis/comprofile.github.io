67d8af5e534c6515f41db846118e78fbc0a06184
thebasis <svrizkyp@gmail.com>
2021-09-16 20:47:10 +0700

    SVR0001

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,23 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+$emailFrom = $_POST['email'];
+$nameFrom = $_POST['name'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$receiving_email_address = 'svrizkyp007@gmail.com';
+include "../assets/vendor/php-email-form/classes/class.phpmailer.php";
+$mail = new PHPMailer;
+$mail->IsSMTP();
+$mail->SMTPSecure = 'ssl';
+$mail->Host = "smtp.gmail.com"; //hostname masing-masing provider email
+// $mail->SMTPDebug = 4;
+$mail->Port = 465;
+$mail->SMTPAuth = true;
+$mail->Username = "uchiharizky2014@gmail.com"; //user email
+$mail->Password = "Kalamani2"; //password email
+$mail->SetFrom($emailFrom, $emailFrom); //set email pengirim
+$mail->Subject = $subject; //subyek email
+$mail->AddAddress($receiving_email_address,'Rizky Pratama'); //tujuan email
+$mail->MsgHTML($message);
+if($mail->Send()) echo 'OK';
+else echo 'Failed to sending message';
 ?>
